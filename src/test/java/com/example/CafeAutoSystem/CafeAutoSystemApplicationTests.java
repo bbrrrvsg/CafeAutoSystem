d1181cd743a45745d8c1d9de20dd1598c146b698
bbrrrvsg <myeonghui1813@gmail.com>
2026-05-28 16:38:40 +0900

    Initial commit

---
+++ src/test/java/com/example/CafeAutoSystem/CafeAutoSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CafeAutoSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeAutoSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
