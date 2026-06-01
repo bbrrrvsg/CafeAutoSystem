539414c573839e0a6a03dbbdbd2c73201a855737
bbrrrvsg <myeonghui1813@gmail.com>
2026-06-01 22:40:17 +0900

    1차 기능 수정중

--- src/main/java/com/example/CafeAutoSystem/AppStart.java
+++ src/main/java/com/example/CafeAutoSystem/AppStart.java
@@ -2,7 +2,9 @@ package com.example.CafeAutoSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // BaseTime 의 @CreatedDate / @LastModifiedDate 자동 주입 활성화
 @SpringBootApplication
 public class AppStart {
     public static void main(String[] args) {
