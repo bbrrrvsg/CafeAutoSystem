1931a68c1f46ba816d77fb9662e4c7b9a4c08c6b
Min0522-sku <wkdalstj0522@gmail.com>
2026-06-04 09:50:39 +0900

    feat: AI발주관리 페이지 연동

--- src/main/java/com/example/CafeAutoSystem/jms_ai_rpa/dto/OrderItemDto.java
+++ src/main/java/com/example/CafeAutoSystem/jms_ai_rpa/dto/OrderItemDto.java
@@ -1,13 +1,17 @@
 package com.example.CafeAutoSystem.jms_ai_rpa.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
+@Builder
 @AllArgsConstructor
 @Getter
 @Setter
+@NoArgsConstructor
 public class OrderItemDto {
     private String ingredientName; // 자재명
     private int orderQty; // 발주 수량
+    private int predictedRequiredQty; // 예상 필요량
+    private int currentStock;         // 현재고
+    private int unitPrice;            // 단가
+    private int totalPrice;           // 예상 금액 (발주 제안량 * 단가)
 }
